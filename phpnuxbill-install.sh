9d1aae7808174b80b81d84aa0d17aab1fdee1c82
Marully <30123569+DzikrRully@users.noreply.github.com>
2025-05-16 11:09:03 +0700

    Update phpnuxbill-install.sh

--- phpnuxbill-install.sh
+++ phpnuxbill-install.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
-
 set -e
 
-echo "Downloading PHPNuxBill..."
-curl -L -o phpnuxbill.zip https://github.com/DzikrRully/phpnuxbill/archive/refs/heads/master.zip
+echo "Installing PHPNuxBill..."
 
-echo "Extracting PHPNuxBill..."
-unzip phpnuxbill.zip
-mv phpnuxbill-master/* /var/www/html/
+cd /var/www/html
 
-echo "Cleaning up installation files..."
-rm -rf phpnuxbill.zip phpnuxbill-master
+# Only install if not already installed
+if [ ! -f index.php ]; then
+  curl -L -o phpnuxbill.zip https://github.com/DzikrRully/phpnuxbill/archive/refs/heads/master.zip
+  unzip phpnuxbill.zip
+  mv phpnuxbill-master/* .
+  rm -rf phpnuxbill.zip phpnuxbill-master
 
-echo "Setting permissions..."
-chown -R www-data:www-data /var/www/html
-chmod -R 755 /var/www/html
+  chown -R www-data:www-data /var/www/html
+  chmod -R 755 /var/www/html
 
-echo "PHPNuxBill installation complete."
+  echo "PHPNuxBill installed."
+else
+  echo "PHPNuxBill already installed, skipping."
+fi
